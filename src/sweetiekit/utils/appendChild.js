ec1cf659d5851e2ecb2a3531e05eab414155cdbf
Emily Kolar <emkolar@gmail.com>
2019-06-06 21:08:19 -0500

    organization

---
+++ src/sweetiekit/utils/appendChild.js
@@ -0,0 +1,11 @@
+export default (parent, child) => {
+  if (parent.addSubview) {
+    parent.addSubview(child);
+  } else if (parent.view && parent.view.addSubview) {
+    parent.view.addSubview(child);
+  } else if (parent.present && child.present) {
+    parent.present(child, true, () => {});
+  } else if (parent.pushViewController && child.present) {
+    parent.pushViewController(child);
+  }
+};

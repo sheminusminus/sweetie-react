f9858e6cd4c4dc9afbb52e6cfb3b0709a968cfa2
Emily Kolar <emkolar@gmail.com>
2019-06-07 05:23:29 -0500

    hot reloading everywhere:

--- src/index.js
+++ src/index.js
@@ -12,24 +12,39 @@ import loadRenderer from './sweetiekit/renderers';
 
 import App from './demo/App';
 
+let platformRender;
+let app;
+let rootVC;
+let root;
+
 function render() {
-  loadRenderer(global.__PLATFORM__).then((module) => {
-    const { render: Render } = module.default;
-
+  if (platformRender) {
     if (global.__PLATFORM__ === 'ios') {
-      const { UIApplication, UIViewController } = SweetieKit;
-
-      const app = new UIApplication();
-      const rootVC = new UIViewController();
-      app.keyWindow.setRootViewController(rootVC);
-
-      Render(<App frame={rootVC.view.frame} />, rootVC);
+      platformRender(<App frame={rootVC.view.frame} />, rootVC);
     } else {
-      const root = document.getElementById('app');
-      console.log(root);
-      Render(<App frame={{ x: 0, y: 0, width: 100, height: 100 }} />, root);
+      platformRender(<App frame={{ x: 0, y: 0, width: 100, height: 100 }} />, root);
     }
-  });
+  } else {
+    loadRenderer(global.__PLATFORM__).then((module) => {
+      const { render: Render } = module.default;
+
+      platformRender = Render;
+
+      if (global.__PLATFORM__ === 'ios') {
+        const { UIApplication, UIViewController } = SweetieKit;
+
+        app = new UIApplication();
+        rootVC = new UIViewController();
+        app.keyWindow.setRootViewController(rootVC);
+
+        Render(<App frame={rootVC.view.frame} />, rootVC);
+      } else {
+        root = document.getElementById('app');
+        console.log(root);
+        Render(<App frame={{ x: 0, y: 0, width: 100, height: 100 }} />, root);
+      }
+    });
+  }
 }
 
 render();
